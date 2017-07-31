2b667f2ef88c81718b2e4e6686807c58894e267f
olaznog10 <olaznog10@gmail.com>
2017-07-31 23:39:07 +0200

    Multiuser first aproach.

--- bot-timpik.py
+++ bot-timpik.py
@@ -1,10 +1,12 @@
 import requests, json
 from dateutil import rrule
 from datetime import datetime, timedelta
+from random import randint
 
 #cuidado por si hay que hacer url encoding
-user = 'olaznog10@gmail.com'
-password = '######'
+user = ['olaznog10@gmail.com', 'olaznog10@gmail.com', 'olaznog10@gmail.com']
+password = ['######', '######', '######']
+cookies = ['', '', '']
 
 headers = {
     'content-type': 'application/x-www-form-urlencoded',
@@ -12,17 +14,17 @@ headers = {
 }
 
 
-def login():
-    url = 'http://www.timpik.com/login?user=' + user + '&pass=' + password + '&recordarme=1&logear='
+def login(pos):
+    url = 'http://www.timpik.com/login?user=' + user[pos] + '&pass=' + password[pos] + '&recordarme=1&logear='
     response = requests.request('POST', url, headers=headers)
     print('login', response)
-    return response.cookies
+    cookies[pos] = response.cookies
 
 
-def find_by_day(date, cookies):
+def find_by_day(date, pos):
     #El cero de provincia creo que es la tuya actual.
     url = 'http://www.timpik.com/events?interestingGamesGetData=1&bt3=1&date=' + str(date) + '&sport=undefined&filter=2&provincia=0'
-    response = requests.request('POST', url, headers=headers, cookies=cookies)
+    response = requests.request('POST', url, headers=headers, cookies=cookies[pos])
     json_events = get_events(response.text.split('\n'))
     event_ids = []
     print('Buscando en el día', current_date, response)
@@ -30,33 +32,36 @@ def find_by_day(date, cookies):
         #Aqui hay que mejorar para que solo se añada los eventos que queremos
         if 'Completo' not in event['evento']['infoCapacity']:
             #120 es gea
-            if event['evento']['pro'] != None and event['evento']['pro']['id'] == 120:
+            if event['evento']['pro'] is not None and event['evento']['pro']['id'] == 120:
                 print(current_date, 'Unirme', event['evento']['name'])
-                join_event(event['evento']['id'])
+                for i in range(len(user) - 1):
+                    if cookies[i] is None:
+                        login(i)
+                    join_event(event['evento']['id'], i)
             else:
                 print(current_date,'Pregunta', event['evento']['name'])
         else:
-            print(current_date,'Completo', event['evento']['name'])
-    return event_ids
-
+            print(current_date, 'Completo', event['evento']['name'])
 
 def get_events(lines):
     for line in lines:
         line = line.strip()
         if 'datosEventos' in line:
+            #el 19 es para quitar la inicializacion y el -1 quita el ;
             return json.loads(line[19:-1])
 
 
-def join_event(id):
+def join_event(id, pos):
     url = 'http://www.timpik.com/events?joinGame'
     payload = 'evento_id=' + str(id) + '&m=1&update=Aceptar'
-    response = requests.request('POST', url, data=payload, headers=headers, cookies=cookies)
+    response = requests.request('POST', url, data=payload, headers=headers, cookies=cookies[pos])
     print(response.text)
 
 
 if __name__ == '__main__':
-    cookies = login()
+    pos = randint(0, len(user)-1)
+    login(pos)
     start_date = datetime.today()
-    ten_days = start_date + timedelta(days=10)
-    for current_date in rrule.rrule(rrule.DAILY, dtstart=start_date, until=ten_days):
-        find_by_day(int(current_date.timestamp()*1000), cookies)
+    next_days = start_date + timedelta(days=3)
+    for current_date in rrule.rrule(rrule.DAILY, dtstart=start_date, until=next_days):
+        find_by_day(int(current_date.timestamp()*1000), pos)
